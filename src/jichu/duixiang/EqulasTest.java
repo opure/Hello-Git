62d239c43ced7ad6075bd7899b7d3548baeb37a8
hao <caohao201308@hotmail.com>
2014-10-17 20:34:54 +0800

    10.17

---
+++ src/jichu/duixiang/EqulasTest.java
@@ -0,0 +1,24 @@
+package jichu.duixiang;
+
+public class EqulasTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="a";
+		String b="a";
+		String c=a+b;
+		String d=a;
+		String e=a+"a";
+		String f="aa";
+		String g =new String("a");
+		
+		System.out.println(a==d);
+		System.out.println(c==f);
+		System.out.println(a.equals(a));
+		System.out.println(a==b);
+		System.out.println(a==b);
+
+
+	}
+
+}
